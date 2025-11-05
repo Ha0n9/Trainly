19abe62708cf1dc85700534618001619bb6b7014
Cristobal R <cromeroaedo@my.bcit.ca>
2025-11-04 18:45:49 -0800

    activities UI

---
+++ app/src/main/java/com/example/trainly/ChangePasswordActivity.java
@@ -0,0 +1,48 @@
+package com.example.trainly;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_change_password);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //references
+        Button btnGoBackToMain=findViewById(R.id.btnBackToMainPassword);
+        Button btnChangePassWord=findViewById(R.id.btnChangePassword);
+
+        //go to main button
+        btnGoBackToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToMainFromPassword= new Intent(ChangePasswordActivity.this,ClientProfileActivity.class);
+                startActivity(goToMainFromPassword);
+            }
+        });
+
+        //password change button listener
+        btnChangePassWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
